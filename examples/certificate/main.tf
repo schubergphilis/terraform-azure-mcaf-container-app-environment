6df3f15b0603242cda60bbf4896934fb95737536
Niko Izsak <nizsak@schubergphilis.com>
2026-05-07 18:56:36 +0200

    update required Terraform version and provider constraints; adjust certificate configuration format

--- examples/certificate/main.tf
+++ examples/certificate/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.10.5"
+  required_version = ">= 1.9"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.16"
+      version = ">= 4.16, < 5.0"
     }
   }
 }
@@ -14,18 +14,17 @@ module "container_app_environment" {
   name                = "my-cae"
   resource_group_name = "my-rg"
   location            = "West Europe"
-  workload_profile = {
-    name = "Consumption"
-    type = "Consumption"
-  }
 
   infrastructure_resource_group_name = "infra-rg"
   infrastructure_subnet_id           = "subnet-id"
   log_analytics_workspace_id         = "log-analytics-workspace-id"
+  logs_destination                   = "log-analytics"
 
-  certificate = {
-    name        = "cert-name"
-    blob_base64 = "base64-encoded-certificate"
-    password    = "certificate-password"
-  }
+  certificates = [
+    {
+      name        = "cert-name"
+      blob_base64 = "base64-encoded-certificate"
+      password    = "certificate-password"
+    }
+  ]
 }
